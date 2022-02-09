2c84f61539babad0b6e217b63fe5aa5564ec415b
Spencercv7 <spencercv7@gmail.com>
2022-02-09 16:42:59 -0500

    Added Google Analytics tacking.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
 import styled, { ThemeProvider } from "styled-components";
-
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import GlobalStyle from "../theme/globalStyles";
 import theme from "../theme/theme";
 import Nav from "../components/layout/nav/Nav";
 import Footer from "../components/layout/footer";
+
+import { pageview } from "../utils/Analytics";
+
 
 const SITEWRAPPER = styled.div`
     position: relative;
@@ -14,6 +18,24 @@ const SITEWRAPPER = styled.div`
 `;
 
 export default function App({ Component, pageProps }: AppProps) {
+
+	const router = useRouter();
+	
+	useEffect(() => {
+		  const handleRouteChange = (url: URL) => {
+			pageview(url);
+		};
+
+		console.log("HERE")
+
+		router.events.on("routeChangeComplete", handleRouteChange);
+
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+
+	}, [router.events])
+
     return (
 		<>
 			<ThemeProvider theme={theme}>
